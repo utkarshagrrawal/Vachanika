f842e89794951a7810a779c355fc96fe197bd6f8
utkarshagrrawal <utkarshagrawal09jan@gmail.com>
2024-12-11 09:33:21 +0530

    fix: enhance error handling in middleware to log panic details for better debugging

--- backend/api/middlewares/errorMiddleware.go
+++ backend/api/middlewares/errorMiddleware.go
@@ -1,13 +1,16 @@
 package middlewares
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func HandlePanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
+				log.Printf("Recovered. Error: %s, Request: %s, Path: %s", err, r.Method, r.URL.Path)
 				http.Error(w, "Internal server error", http.StatusInternalServerError)
-				return
 			}
 		}()
 		next.ServeHTTP(w, r)
